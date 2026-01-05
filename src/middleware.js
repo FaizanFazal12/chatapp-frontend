dfe4b8dc59e71508cae1680c9de17d50b0b7ed23
FaizanFazal12 <fazfaizan22@gmail.com>
2026-01-05 19:22:55 +0500

    fix

--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,21 @@
-// import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-// export const config = {
-//   matcher: ['/', '/dashboard', '/((?!_next|api|favicon.ico).*)'],
-// };
+export const config = {
+  matcher: ['/', '/dashboard', '/((?!_next|api|favicon.ico).*)'],
+};
 
-// export function middleware(req) {
-//   const token = req.cookies.get('token')?.value;
+export function middleware(req) {
+  const token = req.cookies.get('token')?.value;
 
-//   const { pathname } = req.nextUrl;
+  const { pathname } = req.nextUrl;
 
-//   if (token && pathname === '/') {
-//     return NextResponse.redirect(new URL('/dashboard', req.url));
-//   }
+  if (token && pathname === '/') {
+    return NextResponse.redirect(new URL('/dashboard', req.url));
+  }
 
-//   if (!token && pathname.startsWith('/dashboard')) {
-//     return NextResponse.redirect(new URL('/', req.url));
-//   }
+  if (!token && pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
